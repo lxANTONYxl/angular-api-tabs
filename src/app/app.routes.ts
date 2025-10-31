385b3f4b444f94a986f66e7c69b3911751b0d69c
Antony <antony.programmer@gmail.com>
2025-10-31 07:15:03 -0400

    configuracion de los archivos de angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TabsComponent } from './components/tabs/tabs';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TabsComponent },
+  { path: '**', redirectTo: '' }
+];
